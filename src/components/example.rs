bffa16cd5dcecdf31fedced78c5c3feafbb83a82
Tinco Andringa <mail@tinco.nl>
2016-04-07 02:13:11 +0200

    document strategy

--- src/components/example.rs
+++ src/components/example.rs
@@ -1,7 +1,29 @@
 mod example {
 	use std::sync::{ Mutex };
 
-	lazy_static! { static ref List: Mutex<Vec<Component>> = Mutex::new(vec![]); }
+	/// The coolest way to implement this would be to have two lists references.
+	/// One mutable, one is immutable.
+	/// 
+	/// The event system will have a trigger function that takes an
+	/// argument that decides if the event will be triggered this tick
+	/// or the next tick. The default should be next tick to prevent
+	/// inadvertent infinite loop bugs.
+	///
+	/// The event system can then schedule all events handlers that
+	/// do not mutate components to be run in parallel.
+	/// All handlers that do mutate the component list can be run
+	/// sequentially after that. Perhaps we can even schedule
+	/// those with non-overlapping mutable component lists to
+	/// run in parallel.
+	///
+	/// So the next step at least is to make a mutable list iterator
+	/// that we can use and have all events be executed sequentially.
+	///
+	/// An easy way could be that the event loop is responsible for taking
+	/// the lock and all the functions here take a reference to the unlocked
+	/// component list given to them by the event system.
+
+	lazy_static! { static ref List: Mutex<Vec<(usize, Component)>> = Mutex::new(vec![]); }
 
 	/// Example component
 	struct Component {
@@ -9,11 +31,13 @@ mod example {
 		y: f64
 	}
 
-	impl Component {
-		fn create(x: f64, y: f64) {
-			let c = Component { x: x, y: y };
-			let mut list = List.lock().unwrap();
-			list.push(c);
-		}
+	fn create(entity: usize, x: f64, y: f64) {
+		let c = Component { x: x, y: y };
+		let mut list = List.lock().unwrap();
+		list.push((entity,c));
+	}
+
+	fn iter_mut() -> {
+
 	}
 }
