daa9733bc4af6a5cd156f4edd77b1aed0dd2dc7a
Tinco Andringa <mail@tinco.nl>
2016-04-09 23:00:31 +0200

    work out how the event system is going to work a little more

---
+++ src/events/mod.rs
@@ -0,0 +1,34 @@
+/// The event system will have a trigger function that takes an
+/// argument that decides if the event will be triggered this tick
+/// or the next tick. The default should be next tick to prevent
+/// inadvertent infinite loop bugs.
+///
+/// The event system can then schedule all events handlers that
+/// do not mutate components to be run in parallel.
+/// All handlers that do mutate the component list can be run
+/// sequentially after that. Perhaps we can even schedule
+/// those with non-overlapping mutable component lists to
+/// run in parallel.
+
+lazy_static! { pub static ref EventsQueue: Mutex<Vec<> }
+
+/// The event loop should trigger every n ms and execute any
+/// events that are queued up. Every event has a function
+/// that is called by the trigger! macro that puts the event
+/// on the queue.
+pub fn run_loop() {
+
+}
+
+///
+
+/// Defines an event.
+macro_rules! event {
+     
+}
+
+/// 
+macro_rules! trigger {
+	() => ()
+}
+
