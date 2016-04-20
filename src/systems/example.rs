57248952c2c4da46d52438d69d378362da01fbb1
Tinco Andringa <mail@tinco.nl>
2016-04-21 00:02:58 +0200

    start on systems expansion example

---
+++ src/systems/example.rs
@@ -0,0 +1,31 @@
+pub mod my_system {
+	#[derive(Default)]
+	struct State {
+		i: i64
+	}
+
+	pub static ref state: SharedMutex<State> = SharedMutex::new(State::default());
+	
+	impl State {
+		fn event_name(&mut self, data: &Vec<event_name::Data>, positions: &Vec<Position>, descriptions: &mut Vec<Description> ) {
+			let position = positions[0];
+			descriptions[0].set(format!("Position = {},{}", position.x, position.y));
+		}
+	}
+
+	pub fn event_name(&Vec<event_name::Data> event_data, components: Vec<Any>) {
+		let components_iter = components.iter();
+		let positions : &Vec<Position> = components
+			.next().expect("Event components list too short.")
+			.downcast_ref().expect("Event component not of expected type.");
+		let positions : &mut Vec<Description> = components
+			.next().expect("Event components list too short.")
+			.downcast_ref().expect("Event component not of expected type.");
+		let state = state.write().expect("System lock corrupted.");
+		state.event_name(event_data, positions, descriptions);
+	}
+
+	pub fn register() {
+		events::register_handler(event_name, vec![Position::type_id()], vec![Description::type_id()]);
+	}
+}
