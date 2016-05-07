ea7b34f66c38c895a5b75d6100fde43bddb23ae5
Tinco Andringa <mail@tinco.nl>
2016-05-07 16:32:51 +0200

    creating components works

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -3,8 +3,35 @@
 /// over efficiently by systems.
 ///
 /// Whenever a new component is created it is added to a list.
-mod components {
+///
+///
+///
+/// component! { physics, body: physics.RigidBody, physics_id: physics.ID }
+///
 
-}
 
 pub mod example;
+
+#[macro_export]
+macro_rules! component {
+	( $component_name:ident , $( $name:ident : $field:ty ),* ) => (
+		pub mod $component_name {
+			use shared_mutex::{ SharedMutex, SharedMutexWriteGuard };
+			use entity_rust::entities::{ ComponentList, EntityID };
+
+			#[derive(Default)]
+			pub struct Component {
+				pub $($name : $field),*
+			}
+
+			lazy_static! {
+				pub static ref LIST: SharedMutex<ComponentList<Component>> = SharedMutex::new(Vec::new());
+			}
+
+			pub fn create(mut list: SharedMutexWriteGuard<ComponentList<Component>>, entity: EntityID, $($name : $field),*) {
+				let c = Component { $($name : $name),* };
+				list.push((entity,c));
+			}
+		}
+	)
+}
---
+++ tests/components.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate entity_rust;
+#[macro_use]
+extern crate lazy_static;
+extern crate shared_mutex;
+
+component! { test_component, a: i64, b: i64 }
+
+fn reset_state() {
+	let mut state = test_component::LIST.write().expect("Component lock corrupted.");
+	state.clear();
+}
+
+#[test]
+fn generates_functions() {
+	reset_state();
+	{
+		let components = test_component::LIST.write().expect("Component lock corrupted.");
+		test_component::create(components, 1, 2, 3);
+	}
+	{
+		let components = test_component::LIST.read()
+			.expect("Component lock corrupted.");
+		let mut components_iter = components.iter();
+		let component = components_iter.next().expect("Component list too short");
+		assert!(component.0 == 1);
+		assert!(component.1.a == 2);
+	}
+}
