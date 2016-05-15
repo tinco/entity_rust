8e81d825ae82cd86999c8f88d5c366c6e7fcdb73
Tinco Andringa <mail@tinco.nl>
2016-05-15 14:01:45 +0200

    document next steps

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -15,29 +15,18 @@
 /// that is called by the trigger! macro that puts the event
 /// on the queue.
 ///
-/// 2. We need to register handlers.
-/// 4. We need a mechanism for actually receiving the triggers and
-///    handling the special 'tick' behaviour.
-///
-/// Event handlers: 
-/// 
-///  We call them with our arguments, but they need references to
-///  the components they want too. Mega awesome would be if at some
-///  point we can dispatch handlers in parallel depending on whether
-///  they mutate the components.
-///
-///  For this we need to know what components the handler uses and
-///  whether they mutate them or not.
-
 use std::collections::HashSet;
 use std::any::{ Any, TypeId };
 use shared_mutex::{ SharedMutex };
 
 pub mod example;
 
+// The tick queues hold the data associated with the events for each event
 static_any_vec_map! { this_tick_queues, String }
 static_any_vec_map! { next_tick_queues, String }
 
+// The new events sets contain the events that have data in their queues so
+// are ready to be ran.
 lazy_static! {
 	pub static ref THIS_TICK_NEW_EVENTS: SharedMutex<HashSet<String>> = SharedMutex::new(HashSet::new());
 	pub static ref NEXT_TICK_NEW_EVENTS: SharedMutex<HashSet<String>> = SharedMutex::new(HashSet::new());
@@ -55,8 +44,30 @@ pub fn trigger_next_tick<T>(event_name: &String, data: T) where T: Any+'static+S
 	new_events_set.insert(event_name.clone());
 }
 
-pub fn run_loop() {
+// Runs a single iteration of the event system. Can be run in a loop to process events
+// continuously, but should be interleaved with `next_tick` to progress properly.
+pub fn run_events() {
+	// {
+	//   Get (clone) the events for this tick
+	//   Clear the events
+	// }
+	// Then we get the handlers for the ticks
+	// for each handler
+	// {
+	//  	we obtain the correct component locks
+	// 		we run the handlers
+	// }
+}
 
+// Progresses the system to the next tick.
+// Interleave `next_tick` between `run_events` invocations to make sure events that are
+// scheduled to run only once per tick are triggered.
+// A good scheme could be to run `next_tick` every 16ms, while `run_events` is ran continuously.
+pub fn next_tick() {
+	// get NEXT_TICK_NEW_EVENTS
+	// get THIS_TICK_NEW_EVENTS
+	// append NEXT_TICK_NEW_EVENTS to THIS_TICK_NEW_EVENTS
+	// same for queues
 }
 
 #[macro_export]
