d184967cb7c57c9c4b6523809d32361c2be777a2
Tinco Andringa <mail@tinco.nl>
2016-05-23 00:23:03 +0200

    try and implement the lock getter

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -4,21 +4,19 @@
 ///
 /// Whenever a new component is created it is added to a list.
 ///
-///
-///
 /// component! { physics, body: physics.RigidBody, physics_id: physics.ID }
 ///
 
 use std::collections::{ HashSet, HashMap };
 use std::any::{ Any, TypeId };
-use shared_mutex::{ SharedMutex };
+use shared_mutex::{ SharedMutex, SharedMutexReadGuard, SharedMutexWriteGuard };
 
 pub mod example;
 
 #[derive(Clone)]
 pub struct Component {
 	pub name: TypeId,
-	pub get_component_list: fn () -> Box<Any>
+	pub get_component_list: fn () -> Box<Any + 'static>
 }
 
 lazy_static! {
@@ -30,8 +28,12 @@ pub fn register(component : Component) {
 	components.insert(component.name, component);
 }
 
-pub fn get_component_mutex<T>(id : TypeId) -> T {
-
+pub fn get_components_lock<'mutex, T>(id : TypeId) -> SharedMutexReadGuard<'mutex, T> where T : 'static {
+	let components = COMPONENTS.read().expect("COMPONENTS lock corrupted");
+	let component = components.get(&id).expect("Unknown component type requested");
+	let component_list_any = (component.get_component_list)();
+	let component_list : &'static SharedMutex<T> = *component_list_any.downcast_ref().expect("Invalid type for components list");
+	return component_list.read().expect("Component LIST lock corrupted"); 
 }
 
 #[macro_export]
